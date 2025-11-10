95d2a9f8014fdf612a33f08fdfa9bdd88738ea7e
Damian Valencia <damival.32@gmail.com>
2025-11-10 17:46:11 -0600

    Update 02-datatypes.js

---
+++ Basic/02-datatypes.js
@@ -0,0 +1,55 @@
+/// Tipos de datos primitivos ///
+
+// String (cadena de texto)
+
+let name = "Damian Valencia"
+let alias = "Damilobo"
+let email = "damival.32@gmail.com"
+console.log(name)
+console.log(alias)
+console.log(email)
+
+
+// Números
+let age = 20 // Entero
+let height = 1.90 // Decimal
+console.log(age)
+console.log(height)
+
+// Booleanos (bolean)
+let isStudent =  true
+let isProfesor =  false
+console.log(isStudent)
+console.log(isProfesor)
+
+// Undefined
+let undefinedValue
+console.log(undefinedValue)
+
+
+// Null
+let nullValue = null
+console.log(nullValue)
+
+// Symbol
+let mySymbol = Symbol("mysymbol")
+console.log(mySymbol)
+
+// BigInt
+let myBigInt = BigInt(65565465465424321894556522199978998754565465641123265465465465465498498123211)
+let myBigInt2 = 65565465465424321894556522199978998754565465641123265465465465465498498123211n
+console.log(myBigInt)
+console.log(myBigInt2)
+
+// Mostramos los tipos de datos
+
+console.log(typeof name)
+console.log(typeof alias)
+console.log(typeof email)
+console.log(typeof age)
+console.log(typeof height)
+console.log(typeof undefinedValue)
+console.log(typeof nullValue)
+console.log(typeof mySymbol)
+console.log(typeof myBigInt)
+console.log(typeof myBigInt2)

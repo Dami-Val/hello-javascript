87f28c09faf9a2e199d243388f897aa58e07482f
Damian Valencia <damival.32@gmail.com>
2025-11-10 17:27:56 -0600

    Add basic examples for console output and variables
    Introduced two new files: 00-helloworld.js demonstrates various ways to print output and use comments in JavaScript, and 01-variables.js shows usage of var, let, and const for variable declarations.

---
+++ Basic/00-helloworld.js
@@ -0,0 +1,26 @@
+console.log("Hello JavaScript!")
+console.log('Hello JavaScript!')
+console.log(`Hello JavaScript!`)
+
+// Comentario simple.
+
+/*
+Esto es
+un comentario 
+en varias líneas
+*/
+
+console.log("5")
+console.log(5)
+console.log(5 + 2)
+console.log(5 - 2)
+console.log(5 * 2)
+console.log(5 / 2)
+console.log(5 % 2)
+console.log(5 ** 2)
+
+
+
+
+
+
---
+++ Basic/01-variables.js
@@ -0,0 +1,23 @@
+// var
+var helloWorld = "Hello world"
+console.log(helloWorld)
+
+helloWorld = "Hello again!"
+console.log(helloWorld)
+
+// let
+
+let helloWorld2 = "Hello world 2"
+console.log(helloWorld2)
+
+helloWorld2 = "Hello world 2 again!"
+console.log(helloWorld2)
+
+// const
+
+const helloWorld3 = "Hello world 3"
+console.log(helloWorld3)
+
+// error
+// helloWorld3 = "Hello world 3 again!"
+// console.log(helloWorld3)
